3c77044756a82273c107f05c39b13d1c5602c1a5
AYang <nickily233@gmail.com>
2026-07-08 10:58:49 +0800

    Linux_Advanced

---
+++ Linux_Advanced/2_sr04/2_sr04.c
@@ -0,0 +1,37 @@
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/fs.h>
+#include <linux/poll.h>
+#include <linux/of.h>
+#include <linux/platform_device.h>
+#include <linux/interrupt.h>
+#include <linux/gpio/consumer.h>
+#include <linux/kthread.h>
+#include <linux/sched.h>
+#include <linux/delay.h>
+#include <linux/pid.h>
+
+static int sr04_open(struct file *filp, )
+{
+}
+
+static struct file_operations sr04_fops = {
+    .open = sr04_open,
+    .read = sr04_read,
+};
+
+static int sr04_init(void)
+{
+    printk("SR04 驱动加载成功\n");
+    return 0;
+}
+
+static void sr04_exit(void)
+{
+    printk("SR04 驱动卸载成功\n");
+}
+
+module_init(sr04_init);
+module_exit(sr04_exit);
+
+MODULE_LICENSE("GPL");
